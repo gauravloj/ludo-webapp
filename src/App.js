87a5d1d08abdc3f23df7cf08c9fd649f42843d4a
Gaurav Vijayvargia <6872672+gauravloj@users.noreply.github.com>
2024-07-22 23:26:47 -0700

    Add website wireframe design
    Created basic wrieframe design in Figma
    and added as pdf.
    
    Co-authored-by: Apoorva Ranade <56822679+apoorvaran@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,74 +1,8 @@
 import "./App.css";
+import { GameBoard } from "./components/GameBoard";
 
 export default function App() {
   return (
-    <div className="bg-gray-200 flex justify-center items-center h-screen">
-      <div className="ludo-board w-9/12 h-3/4 grid grid-cols-11 grid-rows-11 border-2 border-black">
-        {/* Top row */}
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="home bg-blue-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-
-        {/* Middle rows (excluding the center) */}
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="home bg-blue-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-
-        {/* Center row */}
-        <div className="center bg-red-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="home bg-blue-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="center bg-red-500"></div>
-
-        {/* Bottom rows (excluding the center) */}
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="home bg-blue-500"></div>
-
-        {/* Bottom row */}
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="home bg-blue-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="safe-zone bg-yellow-500"></div>
-        <div className="square"></div>
-        <div className="triangle bg-green-500"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-      </div>
-    </div>
+    <GameBoard />
   );
 }
---
+++ src/components/GameBoard.js
@@ -0,0 +1,5 @@
+export function GameBoard() {
+
+  return <div className="">GameBoard</div>;
+}
+
