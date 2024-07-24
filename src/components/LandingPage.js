603fb1a8f9e4c16a9e752654a149a43e30360300
Gaurav Vijayvargia <6872672+gauravloj@users.noreply.github.com>
2024-07-23 22:32:18 -0700

    Add layout for landing page tags

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,15 @@
 export function LandingPage() {
   return (
-    <>
+    <div className="flex flex-col">
       <div className="">Welcome to the world of Ludo</div>
       <div className="">Lets start a new game!</div>
       <div className=""> Choose your lucky color:</div>
-      <button className="rounded-full bg-lime-600 h-20 w-20"></button>
-      <button className="rounded-full bg-lime-600 h-20 w-20"></button>
-      <button className="rounded-full bg-lime-600 h-20 w-20"></button>
-      <button className="rounded-full bg-lime-600 h-20 w-20"></button>
-    </>
+      <div className="flex flex-row">
+        <button className="rounded-full bg-lime-600 h-20 w-20"></button>
+        <button className="rounded-full bg-lime-600 h-20 w-20"></button>
+        <button className="rounded-full bg-lime-600 h-20 w-20"></button>
+        <button className="rounded-full bg-lime-600 h-20 w-20"></button>
+      </div>
+    </div>
   );
 }
