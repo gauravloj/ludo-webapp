4a8879671dede03746e30b8d4889fa3b6e17dce5
Gaurav Vijayvargia <6872672+gauravloj@users.noreply.github.com>
2024-07-28 23:51:59 -0700

    Add game instructions
    Co-authored-by: Apoorva Ranade <56822679+apoorvaran@users.noreply.github.com>

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,8 +10,27 @@ export function LandingPage({ startGameHandler }) {
       <div className="text-7xl text-center">Welcome to the world of Ludo</div>
       <div className="flex">
         <div className="flex flex-col border-solid border-2 border-sky-500 space-y-4 m-4 p-4">
-          <div className="">How to Play</div>
-          <div className="">Instruction 1</div>
+          <div className="text-3xl">How to Play</div>
+          <ol className="list-disc list-inside text-2xl">
+            <li>
+              Pick your favorite color on the right side to start the game
+            </li>
+            <li>Roll the dice to get a number</li>
+            <li>You need to roll a 6 or 1 to unlock the piece</li>
+            <li>If you get a 6, you get another chance to roll the dice</li>
+            <li>Select the piece to move based on your wise strategy</li>
+            <li>Kill the opponent's piece if you land on the same position</li>
+
+            <li> If you land on the colored box, you are safe</li>
+
+            <li>
+              If you land on the same position as your piece, you can stack them
+            </li>
+            <li>
+              Reach the center position with all your pieces to win the game
+            </li>
+            <li>Enjoy the game!</li>
+          </ol>
         </div>
         <div className="flex flex-col border-solid border-2 border-sky-500 space-y-4 m-4 p-4">
           <div className="">Lets start a new game!</div>
